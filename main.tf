3cd42ff396e4bc6e22aa08d153e33ca6f59cee44
Rafal Matyjasz <rmatyjas@cisco.com>
2022-08-08 11:55:22 +0200

    apply changes

--- main.tf
+++ main.tf
@@ -121,3 +121,49 @@ resource "aws_instance" "app" {
     Environment = var.environment
   }
 }
+
+# resource "aws_instance" "app_a" {
+#   ami           = data.aws_ami.amazon_linux.id
+#   instance_type = var.instance_type
+
+#   subnet_id              = module.vpc.private_subnets[0]
+#   vpc_security_group_ids = [module.app_security_group.this_security_group_id]
+
+#   user_data = <<-EOF
+#     #!/bin/bash
+#     sudo yum update -y
+#     sudo yum install httpd -y
+#     sudo systemctl enable httpd
+#     sudo systemctl start httpd
+#     echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
+#     EOF
+
+#   tags = {
+#     Terraform   = "true"
+#     Project     = var.project_name
+#     Environment = var.environment
+#   }
+# }
+
+# resource "aws_instance" "app_b" {
+#   ami           = data.aws_ami.amazon_linux.id
+#   instance_type = var.instance_type
+
+#   subnet_id              = module.vpc.private_subnets[1]
+#   vpc_security_group_ids = [module.app_security_group.this_security_group_id]
+
+#   user_data = <<-EOF
+#     #!/bin/bash
+#     sudo yum update -y
+#     sudo yum install httpd -y
+#     sudo systemctl enable httpd
+#     sudo systemctl start httpd
+#     echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
+#     EOF
+
+#   tags = {
+#     Terraform   = "true"
+#     Project     = var.project_name
+#     Environment = var.environment
+#   }
+# }
